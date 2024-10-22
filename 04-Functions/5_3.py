5d5073ab0e0bdff80233d00fabffea36937accd1
Kacper <kac.kwa.2005@wp.pl>
2024-10-22 14:22:03 +0200

    5.3 nie skonczone

---
+++ 04-Functions/5_3.py
@@ -0,0 +1,18 @@
+###
+# Functions to read any data type from the keyboard
+#
+def input_string(message):
+    result = input(message)
+    return result
+
+def input_integer(message):
+    result = input(message)
+    return result
+
+def input_real(message):
+    result = input(message)
+    return result
+
+def input_boolean(message):
+    result = input(message)
+    return result
--- 04-Functions/converters.py
+++ 04-Functions/converters.py
@@ -4,9 +4,20 @@ def m_to_cm(n):
 def cm_to_m(n):
     return n/100
 
+def cm_to_inches(n):
+    return n/2.54
+
+def feet_to_cm(n):
+    return n*30.48
+
+def inches_to_cm(n):
+    return n*2,54
+
 if __name__ == "__main__":
     # only execute when you run this module
     # so you can test the functions in this place
     print(f'2m = {m_to_cm(2)}cm')
     print(f'532cm = {cm_to_m(532)}m')
-
+    print(f'67cm = {cm_to_inches(67)}inches')
+    print(f'8ft = {feet_to_cm(8)}cm')
+    print(f'125inch = {inches_to_cm(123)}cm')
