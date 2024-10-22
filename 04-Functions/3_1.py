128500bd1c441127822e4f8fb21f8792e8a41932
Kacper <kac.kwa.2005@wp.pl>
2024-10-22 13:37:34 +0200

    4.4 do ogarniecia

---
+++ 04-Functions/3_1.py
@@ -0,0 +1,9 @@
+import math
+square_root = math.sqrt(7)
+print('A square root of 7 is', square_root)
+
+log = math.log(5)
+print(f'natural logarithm of 5 is {log} ')
+
+sine = math.sin(90)
+print(f'math sine of 90 is {sine}')
---
+++ 04-Functions/4_4.py
@@ -0,0 +1,8 @@
+import math
+def sum_digits(number):
+
+
+    
+number = int(input('Enter your number: '))
+number = abs(number)
+number = str(number)
