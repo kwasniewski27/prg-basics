7969fcc2f867ece6a5b8bef9eb5086ecb54c8581
Kacper <kac.kwa.2005@wp.pl>
2024-12-19 14:47:49 +0100

    ..

--- 11-ObjectOrientedProgramming/tv.py
+++ 11-ObjectOrientedProgramming/tv.py
@@ -1,12 +1,13 @@
 class TV:
     def __init__(self):
-        self.is_on = False
+        self.is_on = True
+        self.channel = '1'
     def on(self):
         self.is_on = True
     def off(self):
         self.is_on = False
     def show_status(self):
         if self.is_on:
-            print(f"Tv is on")
+            print(f"Tv is on, channel: {self.channel}")
         else:
             print(f"Tv is off")
