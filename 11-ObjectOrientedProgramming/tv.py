056dcb2e5452faee740a7eee485e463f5a047365
Kacper <kac.kwa.2005@wp.pl>
2024-12-28 13:14:47 +0100

    pmp

--- 11-ObjectOrientedProgramming/tv.py
+++ 11-ObjectOrientedProgramming/tv.py
@@ -1,33 +1,32 @@
 class TV:
     def __init__(self):
         self.is_on = False
-        self.channel = '1'
-        self.channels_tv = []
+        self.channel = 1
+        self.channels = []
     def on(self):
         self.is_on = True
     def off(self):
         self.is_on = False
     def set_channel(self, new_channel_no):
-        if new_channel_no in self.channels_tv:
+        if 1<= new_channel_no<=len(self.channels):
             self.channel = new_channel_no
         else:
-            print('There is no such channel')
+            print('Invalid channel')
     def set_channels(self, channels_list):
-        self.channels_tv = channels_list
+        self.channels = channels_list
     def show_channels(self):
-        if self.channels_tv:
-            print("Availaible channels: ")
-            for number, name in self.channels_tv:
-                print(f'{number}:{name}')
+        if self.channels:
+            print('Available channels:')
+            for index, name in enumerate(self.channels, start=1):
+                print(f'{index}.{name}')
         else:
-            print('There is no channels yet')
+            print('There is no channels')
     def show_status(self):
         if self.is_on:
-            channel_name = ''
-            for number, name in self.channels_tv:
-                if number == self.channel:
-                    channel_name = name
-                    break
-            print(f"Tv is on, channel: {self.channel} ({channel_name})")
+            if 1 <= self.channel <= len(self.channels):
+                channel_name = self.channels[self.channel - 1]
+            else:
+                channel_name = 'no_channel'
+            print(f'Tv is on, channel: {self.channel}, {channel_name}')
         else:
-            print(f"Tv is off")
+            print('Tv is off')
--- test.py
+++ test.py
@@ -1,10 +1,41 @@
-import random
-number = random.randint(1,100)
-your_number = int(input('Wpisz swoją liczbę: '))
-while your_number != number:
-    if your_number>number:
-        print('big big big')
-    elif your_number<number:
-        print('chill chill guy')
-    your_number = int(input('Wpisz swoją liczbę: '))
-print('hahahah')
+class TV:
+    def __init__(self):
+        self.is_on = False
+        self.channel = '1'
+        self.channels_tv = []  # Lista krotek (numer, nazwa)
+
+    def on(self):
+        self.is_on = True
+
+    def off(self):
+        self.is_on = False
+
+    def set_channel(self, new_channel_no):
+        if any(number == new_channel_no for number, _ in self.channels_tv):
+            self.channel = new_channel_no
+        else:
+            print('There is no such channel')
+
+    def set_channels(self, channels_list):
+        # Oczekujemy listy krotek [(numer, nazwa), ...]
+        self.channels_tv = channels_list
+
+    def show_channels(self):
+        if self.channels_tv:
+            print("Available channels: ")
+            for number, name in self.channels_tv:
+                print(f'{number}: {name}')
+        else:
+            print('There are no channels yet')
+
+    def show_status(self):
+        if self.is_on:
+            # Znajdź nazwę kanału
+            channel_name = "Unknown"
+            for number, name in self.channels_tv:
+                if number == self.channel:
+                    channel_name = name
+                    break
+            print(f"Tv is on, channel: {self.channel} ({channel_name})")
+        else:
+            print("Tv is off")
